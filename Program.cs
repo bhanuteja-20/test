fa4a613219b0dd0b16f6d1d76a55bbb1c4f5a4df
bhanuteja-20 <bhanuteja.u20@gmail.com>
2025-05-28 22:27:18 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+class Bike
+{
+  string bikename="ducati";
+  string bikename2="bmw";
+  static void Main(string[] args)
+  {
+    Bike one=new Bike();
+    Bike two=new Bike();
+    Console.WriteLine(one.bikename);
+    Console.WriteLine(two.bikename2);
+  }
+}
